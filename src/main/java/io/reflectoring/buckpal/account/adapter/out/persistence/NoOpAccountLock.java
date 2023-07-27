a8f5828898041ce8e41d2f4eef7209f3abcdd508
jay <jaeykweon@naver.com>
2023-07-27 21:10:18 +0900

    NoOpAccountLock 위치 오류 수정

--- src/main/java/io/reflectoring/buckpal/account/adapter/out/persistence/NoOpAccountLock.java
+++ src/main/java/io/reflectoring/buckpal/account/adapter/out/persistence/NoOpAccountLock.java
@@ -1,4 +1,4 @@
-package io.reflectoring.buckpal.account.application.service;
+package io.reflectoring.buckpal.account.adapter.out.persistence;
 
 import io.reflectoring.buckpal.account.application.port.out.AccountLock;
 import io.reflectoring.buckpal.account.domain.Account.AccountId;
